712a59cea1ddbe52b46b27d03dbf23a3627f12a7
jeffma <jeffma@uw.edu>
2016-08-12 17:47:26 +0000

    cleaned up code and added an open new window for wayback machine link

--- mysite/urls.py
+++ mysite/urls.py
@@ -3,13 +3,9 @@ from django.contrib import admin
 from django.contrib.auth import views
 
 urlpatterns = [
-#    url('^', include('django.contrib.auth.urls')),
     url(r'^admin/', admin.site.urls),
     url(r'^accounts/login/$', views.login, name='login'),
     url(r'^login/$', views.login, name='login'),
-#    url(r'^$', views.login, name='login'),
-#    url('^', include('django.contrib.auth.urls')),
     url(r'^logout/$', views.logout, name='logout', kwargs={'next_page': '/expander2'}), 
     url(r'', include('urlexpander2.urls')),
-#    url(r'^expander/', include('urlexpander2.urls')), 
 ]
